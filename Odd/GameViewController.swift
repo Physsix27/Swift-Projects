47d3bd11c5b1b15d847fd0ac6c962bd42aaa7cce
physsi07 <starlinu33@gmail.com>
2019-04-05 07:26:32 -0400

    updating....

--- Odd/GameViewController.swift
+++ Odd/GameViewController.swift
@@ -9,27 +9,272 @@
 import UIKit
 
 class GameViewController: UIViewController {
-
+   
+    
+    // variable
+    static var selection: Int = 1;
+    var answer: Int = 0
+    var number1: Int = 0
+    var number2: Int = 0
+    var symbol: Character!
+    var mainScore: Int = 0
+    
+    // Main Number
+    @IBOutlet weak var mainRandomImage: UIImageView!
+    
+    // Timer Label
+    @IBOutlet weak var timerLabel: UILabel!
+    
+    // My Number
+    @IBOutlet weak var myNumber: UILabel!
+    
+    @IBOutlet weak var score: UILabel!
+    
+    //
+    @IBOutlet weak var pauseButton: UIButton!
+    @IBAction func pauseButton(_ sender: Any) {
+        
+        if (true) {
+            pausing()
+        }
+    }
+    
+    // An array variable
+    private var NumbersArray = [String]()
+    
+    var timer = Timer() // A timer variable
+    var seconds = 60    // 60 seconds
+    
+    // Views
+    @IBOutlet weak var MainView: UIView!
+    @IBOutlet weak var RightView: UIView!
+    @IBOutlet weak var LeftView: UIView!
+    
+    // Left Images Outlets
+    @IBOutlet weak var LeftUIImage1: UIImageView!
+    @IBOutlet weak var LeftUIImage2: UIImageView!
+    @IBOutlet weak var LeftUIImage3: UIImageView!
+    @IBOutlet weak var LeftUIImage4: UIImageView!
+    
+    // filter  string
+    func  filter(_ sender: String) -> Int {
+        var number: String = ""
+        for character in sender {
+            if(character >= "0" && character <= "9") {
+                // print("character: ", character)
+                number.append(character)
+            }
+        }
+        return Int(number)!
+    }
+    
+    // Check if three variables are set up correctly!
+    func is3VarSetUp()
+    {
+        if(number1 != 0 && number2 != 0 && symbol != nil && answer == nil) {
+            calculate(number1: number1, symbol: symbol, number2: number2)
+            print("answer: ", answer)
+        }
+        else if (GameViewController.selection == 2 && number1 != 0  && symbol != nil && answer != nil)  {
+            calculate(number1: answer, symbol: symbol, number2: number1)
+            print("answer: ", answer)
+        }
+            
+        else if (GameViewController.selection == 1 && number1 != 0 && number2 != 0 && symbol != nil && answer != nil ) {
+            calculate(number1: answer, symbol: symbol, number2: number2)
+            print("answer: ", answer)
+        }
+        myNumber.text = String(answer)
+    }
+    
+    // Function that calculates the numbers
+    func calculate(number1: Int, symbol:Character, number2: Int) {
+        switch (symbol) {
+        case "+":
+            answer = number1 + number2
+            break
+            
+        case "-":
+            answer = number1 - number2
+            break
+            
+        case "*":
+            answer = number1 * number2
+            break
+            
+        case "/":
+            answer = number1 / number2
+            break
+            
+        default:
+            break
+        }
+    }
+    
+    // A function that loads all the images to the array variable
+    private func images(){
+        var word: String
+        for i in 1...100 {
+            word = "Num" + String(i)
+            NumbersArray.append(word)
+        }
+    }
+    
+    // Function that returns an random image from the array
+    private func randomImage() -> String {
+        var number: String
+        number = NumbersArray[Int(arc4random_uniform(100))]
+        return number
+    }
+    
+    // Function that assigns operators
+    @IBAction func handleSigns(_ sender: Any?) {
+        let image = sender as? UITapGestureRecognizer
+        //print("\n", image!.view!.accessibilityIdentifier!)
+        symbol = Character(image!.view!.accessibilityIdentifier!)
+        print(symbol!)
+        is3VarSetUp()
+    }
+    
+    // Function that assings the correct number to my variables
+    @IBAction func handleNumbers(_ sender: Any?) {
+        let image = sender as? UITapGestureRecognizer
+        //print("\n", image!.view!.accessibilityIdentifier!)
+        if (GameViewController.selection == 1) {
+            number1 =  filter(image!.view!.accessibilityIdentifier!)
+            GameViewController.selection += 1
+            print("number1: ", number1)
+            is3VarSetUp()
+        }
+        else if(GameViewController.selection == 2) {
+            number2 = filter(image!.view!.accessibilityIdentifier!)
+            GameViewController.selection = 1
+            print("number2: ", number2)
+            is3VarSetUp()
+            number1 = 0
+            number2 = 0
+            symbol = nil
+        }
+        else {
+            GameViewController.selection = 1
+            number1 = 0
+            number2 = 0
+            symbol = nil
+        }
+        is3VarSetUp()
+    }
+    
+    // Function that assings randomImage() to each UIImage
+    @objc private func the4Images() {
+        
+        var assigned: String
+        
+        assigned = randomImage()
+        LeftUIImage1.accessibilityIdentifier = assigned
+        LeftUIImage1.image = UIImage(named: assigned)
+        
+        assigned = randomImage()
+        LeftUIImage2.accessibilityIdentifier = assigned
+        LeftUIImage2.image = UIImage(named: assigned)
+        
+        assigned = randomImage()
+        LeftUIImage3.accessibilityIdentifier = assigned
+        LeftUIImage3.image = UIImage(named: assigned)
+        
+        assigned = randomImage()
+        LeftUIImage4.accessibilityIdentifier = assigned
+        LeftUIImage4.image = UIImage(named: assigned)
+    }
+    
+    // Function that changes the main number image
+    public func changeNumber() {
+        var set: String
+        
+        set = randomImage()
+        mainRandomImage.accessibilityIdentifier = set
+        mainRandomImage.image = UIImage(named: set)
+    }
+    
+    //**********************************************************************************//
+    
+    // My Timer That Changes my images
+    public func runTimer(){
+        timer = Timer.scheduledTimer(timeInterval: 2, target: self,   selector: (#selector(GameViewController.the4Images)), userInfo: nil, repeats: true)
+    }
+    
+    // Function for the main timer
+    public func clockticking() {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(GameViewController.counter), userInfo: nil, repeats: true)
+    }
+    
+    // Timer for the user to play
+    func counter(){
+        seconds -= 1
+        timerLabel.text = String(seconds)
+        if (seconds == 0) {
+            changeNumber()
+            timer.invalidate()
+            seconds = 60
+            myNumber.text = "0"
+            clockticking()
+        }
+    }
+    
+    //**********************************************************************************//
+    
+    // Function that changes the background every second
+    func changeBackround()  {
+        self.MainView.backgroundColor = UIColor.randomColor()
+    }
+    
+    // Function that resets the timer
+    func resetTimer() {
+        seconds = 60
+    }
+    
+    public func pausing(){
+        timer.invalidate()
+        
+    }
+    
+    // Function that checks if it matches the right number
+    func checking(){
+        if(answer == Int(myNumber.text!)) {
+            mainScore += 2
+            score.text = String(mainScore)
+            changeNumber()
+            resetTimer()
+            print("it matches")
+        }
+    }
+    
+    // Loads eveything when the view loads up
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Calling my functions
+        images()
+        the4Images()
+        runTimer()
+        changeNumber()
+        clockticking()
+        checking()
+        
+        Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(GameViewController.changeBackround), userInfo: nil, repeats: true)
 
-        // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+extension UIColor {
+     static func randomColor() -> UIColor {
+        let red = CGFloat(drand48())
+        let green = CGFloat(drand48())
+        let blue = CGFloat(drand48())
+        return UIColor(red: red, green: green, blue: blue, alpha: 1.0)
     }
-    */
-
 }
--- Odd/MenuViewController.swift
+++ Odd/MenuViewController.swift
@@ -8,18 +8,97 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class MenuViewController: UIViewController {
+    
+    @IBOutlet var Labels: [UIImageView]!
+    
+    @IBOutlet var BoundsImageView: [UIImageView]!
+    @IBOutlet weak var BoundsImageView1: UIImageView!
+    @IBOutlet weak var BoundsImageView2: UIImageView!
+    @IBOutlet weak var BoundsImageView3: UIImageView!
+    @IBOutlet weak var BoundsImageView4: UIImageView!
+    @IBOutlet weak var BoundsImageView5: UIImageView!
+    @IBOutlet weak var BoundsImageView6: UIImageView!
+    @IBOutlet weak var BoundsImageView7: UIImageView!
+    @IBOutlet weak var BoundsImageView8: UIImageView!
+    @IBOutlet weak var BoundsImageView9: UIImageView!
+    
+    
+    @IBOutlet var Buttons: [UIButton]!
+    
+    //Array of my images numbers
+    var NumbersArray: [String] = ["Num1","Num2","Num3","Num4","Num5","Num6","Num7","Num8","Num9","Num10","Num11","Num12","Num13","Num14","Num15","Num16","Num17","Num18","Num19","Num20","Num21","Num22","Num23","Num24","Num25","Num26","Num27","Num28","Num29","Num30","Num31","Num32","Num33","Num34","Num35","Num36","Num37","Num38","Num39","Num40","Num41","Num42","Num43","Num44","Num45","Num46","Num47","Num48","Num49","Num50","Num51","Num52","Num53","Num54","Num55","Num56","Num57","Num58","Num59","Num60","Num61","Num62","Num63","Num64","Num65","Num66","Num67","Num68","Num69","Num70","Num71","Num72","Num73","Num74","Num75","Num76","Num77","Num78","Num79","Num80","Num81","Num82","Num83","Num84","Num85","Num86","Num87","Num88","Num89","Num90","Num91","Num92","Num93","Num94","Num95","Num96","Num97","Num98","Num99","Num100"]
+    
+    
+    //This func change the number in the random order
+    func ChangeImage(_ animated: Bool)
+    {
+        //ImageVIew function to chage the image of the ImageVoew
+        let MainbuttonRandom1:Int = Int(arc4random_uniform(100))
+        let MainButtonString1:String =  self.NumbersArray[MainbuttonRandom1]
+        
+        let MainbuttonRandom2:Int = Int(arc4random_uniform(100))
+        let MainButtonString2:String =  self.NumbersArray[MainbuttonRandom2]
+        
+        let MainbuttonRandom3:Int = Int(arc4random_uniform(100))
+        let MainButtonString3:String =  self.NumbersArray[MainbuttonRandom3]
+        
+        let MainbuttonRandom4:Int = Int(arc4random_uniform(100))
+        let MainButtonString4:String =  self.NumbersArray[MainbuttonRandom4]
+        
+        let MainbuttonRandom5:Int = Int(arc4random_uniform(100))
+        let MainButtonString5:String =  self.NumbersArray[MainbuttonRandom5]
+        
+        let MainbuttonRandom6:Int = Int(arc4random_uniform(100))
+        let MainButtonString6:String =  self.NumbersArray[MainbuttonRandom6]
+        
+        let MainbuttonRandom7:Int = Int(arc4random_uniform(100))
+        let MainButtonString7:String =  self.NumbersArray[MainbuttonRandom7]
+        
+        let MainbuttonRandom8:Int = Int(arc4random_uniform(100))
+        let MainButtonString8:String =  self.NumbersArray[MainbuttonRandom8]
+        
+        let MainbuttonRandom9:Int = Int(arc4random_uniform(100))
+        let MainButtonString9:String =  self.NumbersArray[MainbuttonRandom9]
+        
+        
+        self.BoundsImageView1.image = UIImage(named: MainButtonString1)
+        self.BoundsImageView2.image = UIImage(named: MainButtonString2)
+        self.BoundsImageView3.image = UIImage(named: MainButtonString3)
+        self.BoundsImageView4.image = UIImage(named: MainButtonString4)
+        self.BoundsImageView5.image = UIImage(named: MainButtonString5)
+        self.BoundsImageView6.image = UIImage(named: MainButtonString6)
+        self.BoundsImageView7.image = UIImage(named: MainButtonString7)
+        self.BoundsImageView8.image = UIImage(named: MainButtonString8)
+        self.BoundsImageView9.image = UIImage(named: MainButtonString9)
+    }
+    
+    //
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Calling the ChangeImage Function
+        ChangeImage(true)
+        
+        let Option = UIViewAnimationOptions.repeat
+        
+        UIView.animate(withDuration: 2.5, delay: 0.0, options: Option, animations: {
+            
+            self.BoundsImageView1.frame = CGRect(x: 1200 - 35, y: -320 - 1235 , width: 50, height: 50)
+            self.BoundsImageView2.frame = CGRect(x: 120 - 550, y: 220 , width: 50, height: 330)
+            
+            self.BoundsImageView3.frame = CGRect(x: 420 - 50, y: -220 , width: 50, height: 330)
+            self.BoundsImageView4.frame = CGRect(x: -120 - 550, y: -220, width: 0, height: 330)
+            
+            self.BoundsImageView5.frame = CGRect(x: -320-50, y: -220 , width: 50, height: 50)
+            self.BoundsImageView6.frame = CGRect(x: -420-50, y: -220 , width: 50, height: 330)
+            
+            self.BoundsImageView7.frame = CGRect(x: 1200 - 35, y: -320 - 1235 , width: 50, height: 50)
+            self.BoundsImageView8.frame = CGRect(x: 120-550, y: 220 , width: 50, height: 330)
+            
+            self.BoundsImageView9.frame = CGRect(x: 720 , y: -220 , width: 50, height: 330)
+            
+        }, completion: nil)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
